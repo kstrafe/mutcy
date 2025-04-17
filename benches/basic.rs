790e2decfd296bebacf41bf006a160f40de4132f
Kevin Robert Stravers <kevin@stravers.net>
2025-04-16 20:16:09 -0400

    Add benchmark for counting

--- benches/basic.rs
+++ benches/basic.rs
@@ -24,6 +24,44 @@ fn criterion_benchmark(c: &mut Criterion) {
             }
         });
     });
+
+    c.bench_function("recursion-native", |bench| {
+        fn ping_pong(a: &mut i32, b: &mut i32) {
+            if *a > 0 {
+                *a -= black_box(1);
+                *b += black_box(1);
+
+                ping_pong(a, b);
+            }
+        }
+
+        bench.iter(|| {
+            let mut a = 1000;
+            let mut b = 0;
+            ping_pong(&mut a, &mut b);
+        });
+    });
+
+    c.bench_function("recursion-keycell", |bench| {
+        fn ping_pong(a: KeyMut<i32>, b: &KeyCell<i32>) {
+            if **a > 0 {
+                **a -= black_box(1);
+
+                let (_, key) = Key::split(a);
+                *b.borrow_mut(key) += black_box(1);
+
+                ping_pong(a, b);
+            }
+        }
+
+        let mut key = Key::acquire();
+
+        bench.iter(|| {
+            let a = KeyCell::new(1000, ());
+            let b = KeyCell::new(0, ());
+            ping_pong(&mut a.borrow_mut(&mut key), &b);
+        });
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
