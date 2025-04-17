757f2ecf9c018341ec7d8478439e4b93b505bf43
Kevin Robert Stravers <kevin@stravers.net>
2025-04-16 20:22:29 -0400

    Group benchmarks, bump version

--- benches/basic.rs
+++ benches/basic.rs
@@ -1,67 +1,83 @@
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use criterion::{Criterion, Throughput, black_box, criterion_group, criterion_main};
 use mutcy::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("count-1M-native", |bench| {
-        let mut x = 0;
-        bench.iter(|| {
-            x = 0;
-            for _ in 0..1_000_000 {
-                x += black_box(1);
-            }
+    {
+        let mut group = c.benchmark_group("count");
+        const COUNT: u64 = 1_000_000;
+        group.throughput(Throughput::Elements(COUNT));
+
+        group.bench_function("native", |bench| {
+            let mut x = 0;
+            bench.iter(|| {
+                x = 0;
+                for _ in 0..COUNT {
+                    x += black_box(1);
+                }
+            });
         });
-    });
 
-    c.bench_function("count-1M-keycell", |bench| {
-        let mut key = Key::acquire();
+        group.bench_function("keycell", |bench| {
+            let mut key = Key::acquire();
 
-        let x = KeyCell::new(0usize, ());
+            let x = KeyCell::new(0usize, ());
 
-        bench.iter(|| {
-            *x.borrow_mut(&mut key) = 0;
-            for _ in 0..1_000_000 {
-                *x.borrow_mut(&mut key) += black_box(1);
-            }
+            bench.iter(|| {
+                *x.borrow_mut(&mut key) = 0;
+                for _ in 0..COUNT {
+                    *x.borrow_mut(&mut key) += black_box(1);
+                }
+            });
         });
-    });
 
-    c.bench_function("recursion-native", |bench| {
-        fn ping_pong(a: &mut i32, b: &mut i32) {
-            if *a > 0 {
-                *a -= black_box(1);
-                *b += black_box(1);
+        group.finish();
+    }
 
-                ping_pong(a, b);
+    {
+        let mut group = c.benchmark_group("recursion");
+        const COUNT: u64 = 1000;
+        group.throughput(Throughput::Elements(COUNT));
+
+        group.bench_function("native", |bench| {
+            fn ping_pong(a: &mut i32, b: &mut i32) {
+                if *a > 0 {
+                    *a -= black_box(1);
+                    *b += black_box(1);
+
+                    ping_pong(a, b);
+                }
             }
-        }
 
-        bench.iter(|| {
-            let mut a = 1000;
-            let mut b = 0;
-            ping_pong(&mut a, &mut b);
+            bench.iter(|| {
+                let mut a = 1000;
+                let mut b = 0;
+                ping_pong(&mut a, &mut b);
+            });
         });
-    });
 
-    c.bench_function("recursion-keycell", |bench| {
-        fn ping_pong(a: KeyMut<i32>, b: &KeyCell<i32>) {
-            if **a > 0 {
-                **a -= black_box(1);
+        group.bench_function("keycell", |bench| {
+            fn ping_pong(a: KeyMut<i32>, b: &KeyCell<i32>) {
+                if **a > 0 {
+                    **a -= black_box(1);
 
-                let (_, key) = Key::split(a);
-                *b.borrow_mut(key) += black_box(1);
+                    let (_, key) = Key::split(a);
+                    *b.borrow_mut(key) += black_box(1);
 
-                ping_pong(a, b);
+                    ping_pong(a, b);
+                }
             }
-        }
 
-        let mut key = Key::acquire();
+            let mut key = Key::acquire();
 
-        bench.iter(|| {
-            let a = KeyCell::new(1000, ());
-            let b = KeyCell::new(0, ());
-            ping_pong(&mut a.borrow_mut(&mut key), &b);
+            bench.iter(|| {
+                let a = KeyCell::new(1000, ());
+                let b = KeyCell::new(0, ());
+                ping_pong(&mut a.borrow_mut(&mut key), &b);
+            });
         });
-    });
+
+        group.finish();
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
